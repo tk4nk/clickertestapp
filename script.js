5a151614e821f773db2a57b8d92586b32146252a
tk4nk <tk4nk3537@gmail.com>
2024-06-24 15:11:02 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ let balance = 0;
 document.getElementById('btn').onclick = click;
 
 function click() {
+    let audio = new Audio("click.mp3");
+    audio.play();
+    
     balance++;
-    document.getElementById('bal').innerHTML = `clicks: ${balance}`;
+    document.getElementById('bal').innerHTML = `balance: ${balance}`;
+    
+    setTimeout(() => {
+        document.getElementById('btn').style.transform = "none";
+    }, 250);
+    
+    
+    document.getElementById('btn').style.transform = "scale(0.94)";
 }
