abc72390353898d509f4201d10fdfe97079be610
tk4nk <tk4nk3537@gmail.com>
2024-06-25 18:22:51 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let tg = window.Telegram;
+if (tg != undefined) {
+    if (tg.WebApp != undefined && tg.WebApp.initData != undefined) {
+        let data = tg.WebApp.initData;
+
+        console.log(data)
+        console.log("используется тг клиент");
+        tg.WebApp.backgroundColor = '#3d3d3d';
+        tg.WebApp.headerColor = '#212121';
+        tg.WebApp.expand();
+    }
+} else {
+    console.log("используется не тг клиент");
+}
+
 // console.log(window.Telegram.WebApp.colorScheme);
 let balance = 0;
 
